82f345de6f09ee04d7c76bfa348a871a5ac802bc
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-20 19:03:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+#def reverse_each_word(string)
+#  string_array = string.split(' ')
+#  reversed_array = []
+#  string_array.each do |words|
+#    reversed_array << words.reverse
+#  end
+#  reversed_array.join(' ')
+#end
+
+def reverse_each_word(string)
+  string_array = string.split(' ')
+  reversed_array = []
+    string_array.collect do |words|
+      reversed_array << words.reverse
+    end
+  reversed_array.join(' ')
+end
