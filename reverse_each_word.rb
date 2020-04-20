678221b26b3431f3e99e4ff2171fef66b617c575
Austin Buhler <austinbuhler@gmail.com>
2020-04-20 20:35:12 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,9 @@ end
 
 def reverse_each_word(string)
   str_array = string.split
+  return_array = []
   str_array.collect do |word|
-    word.reverse
+    return_array << word.reverse
   end
+  return_array.join(" ")
 end
