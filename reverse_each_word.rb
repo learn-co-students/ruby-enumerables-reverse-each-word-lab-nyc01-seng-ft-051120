bc4e0d6f9ed75c66a93b7f0aea9a2776716af20d
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-26 19:01:03 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 
 def reverse_each_word(str)
-  arr= str.split
-  new_str=arr.collect { |e| e.reverse
+  str.each{ |s|
+str=[s," "]
+  }
+  new_str=str.collect { |e| e.reverse
 
  }
  new_str.join(" ")
